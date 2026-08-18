b986ca5bc2196b0f3ea1354eea635ff5341e2b50
Cauã de Lima Araújo <20231144010003@ifrn.local>
2026-08-18 16:15:45 -0300

    feat: adiciona template semi-funcional para rota notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ConsultaDeputados } from './components/consulta-deputados/consulta-deputados';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
-    { path: '', component: ConsultaDeputados }
+    { path: '', component: ConsultaDeputados },
+    { path: '**', component: NotFound }
 ];
